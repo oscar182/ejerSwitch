7e774f3cf488b70517fac3ba3f3ad65e5e23e3df
oscar182 <oscardamianfranco@gmail.com>
2016-11-16 10:33:14 -0300

    Ejercicios de PHP switch

---
+++ switch.php
@@ -0,0 +1,8 @@
+<?php
+
+echo "<b>Ejercicio 1</b><br>Bienvenido al puestito de verduleria digital: <br><br>Por favor ingrese un numero para elejir lo que quiere comprar:<br>1.- Bananas.<br>2.- Manzanas.<br>3.-Peras.<br>4.- Ensalada de frutas.<br><br><br>";
+echo "<b>Ejercicio 2</b><br>Desea cerrar la operacion? <br><br>Por favor ingrese un numero:<br>1.- Si<br>2.-No <br>en caso de que sea otro valor que diga por favor vuelva a ingresar un valor";
+echo "<br><br>";
+echo "<b>Ejercicio 3</b><br>Elija el pack a su gusto: <br><br>Por favor ingrese un numero para elejir lo que quiere comprar:<br>1.- Servicio de internet<br>2.- Servicio de Cable<br>3.-Servicio de Telefono<br>4.-No desea ninguno?.<br><br><br>Si no desea ninguno que imprima 'Muchas gracias lo esperamos pronto', si elijio un pack, 'muy buena eleccion'";
+
+?>
